f7622808c7562e3611697764d92562a22b583318
Omkar Kashyap <omkar.m.kashyap@gmail.com>
2023-10-22 11:56:30 +0530

    Changed the tensor shape in transformer_encoder.py to work

--- encoder/transformer_encoder.py
+++ encoder/transformer_encoder.py
@@ -17,11 +17,13 @@ from data.dataset import QGDataset, dataloader
 d_model = 512  
 num_heads = 8 
 seq_len = 128  
-vocab_size = 10000
+vocab_size = 100000
 num_token_types = 2  
-batch_size = 32
+batch_size = 4
 
 dataset = QGDataset(root_dir="hotpot/data", max_seq_length=128)
+
+# Initialize and load the model
 model = TransformerModel(d_model, num_heads, seq_len, vocab_size, num_token_types)
 dataloader = DataLoader(
     dataset=dataset,
@@ -31,26 +33,26 @@ dataloader = DataLoader(
 )
 
 for batch in dataloader:
-    input_ids = batch['input_ids']    
+    input_ids = batch['input_ids']  
+    input_ids = input_ids.view(4, 128)
+    
     token_type_ids = batch['token_type_ids']
+    token_type_ids = token_type_ids.view(4, 128)
+    
     attention_mask = batch['attention_mask']
-    print(input_ids.shape)
-    # output = model(input_ids, token_type_ids, attention_mask)
-    # print(output)
+    attention_mask = attention_mask.view(4, 128)
+    
+    # Perform a forward pass
+    output = model(input_ids, token_type_ids, attention_mask)
+    print(output)
     
 #--------------------
 
-# Prepare sample data
-sample_input_ids = torch.randint(0, vocab_size, (batch_size, seq_len))
-sample_input_ids = sample_input_ids.view(4,1,128)
-sample_token_type_ids = torch.randint(0, num_token_types, (batch_size, seq_len))
-sample_mask = torch.ones(batch_size, seq_len)
-
-print(sample_input_ids.shape)
-
-# Initialize and load the model
-model = TransformerModel(d_model, num_heads, seq_len, vocab_size, num_token_types)
-
-# Perform a forward pass
-output = model(sample_input_ids, sample_token_type_ids, sample_mask)
-print(output)
+# sample_input_ids = torch.randint(0, vocab_size, (batch_size, seq_len))
+# sample_input_ids = sample_input_ids.view(4, 128)
+# sample_token_type_ids = torch.randint(0, num_token_types, (batch_size, seq_len))
+# sample_mask = torch.ones(batch_size, seq_len)
+# print(sample_input_ids.shape , sample_token_type_ids.shape, sample_mask.shape)
+# model = TransformerModel(d_model, num_heads, seq_len, vocab_size, num_token_types)
+# output = model(sample_input_ids, sample_token_type_ids, sample_mask)
+# print(output)
